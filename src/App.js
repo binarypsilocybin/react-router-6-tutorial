d9414df07911e48da477a80cbad897d0254d1866
binarypsilocybin <rogeriosvaldo@gmail.com>
2022-10-08 17:09:20 +0100

    New navbar

--- src/App.js
+++ src/App.js
@@ -7,12 +7,15 @@ import Error from "./pages/Error";
 function App() {
   return (
     <BrowserRouter>
+      <nav>our navbar</nav>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="products" element={<Products />} />
-        <Route path="*" element={<Error />} />
+        <Route path="/" element={<Home />}>
+          <Route path="about" element={<About />} />
+          <Route path="products" element={<Products />} />
+          <Route path="*" element={<Error />} />
+        </Route>
       </Routes>
+      <footer>our footer</footer>
     </BrowserRouter>
   );
 }
