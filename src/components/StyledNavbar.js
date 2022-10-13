196be0222d92663d8bccdf0972235ae698e0ec85
binarypsilocybin <rogeriosvaldo@gmail.com>
2022-10-13 16:49:54 +0100

    Login register

--- src/components/StyledNavbar.js
+++ src/components/StyledNavbar.js
@@ -1,28 +1,32 @@
 import { NavLink } from "react-router-dom";
-
 const Navbar = () => {
   return (
     <nav className="navbar">
       <NavLink
         to="/"
-        style={({ isActive }) => (isActive ? "link active" : "link")}
+        className={({ isActive }) => (isActive ? "link active" : "link")}
       >
         Home
       </NavLink>
       <NavLink
         to="/about"
-        style={({ isActive }) => (isActive ? "link active" : "link")}
+        className={({ isActive }) => (isActive ? "link active" : "link")}
       >
         About
       </NavLink>
       <NavLink
         to="/products"
-        style={({ isActive }) => (isActive ? "link active" : "link")}
+        className={({ isActive }) => (isActive ? "link active" : "link")}
       >
         Products
+      </NavLink>
+      <NavLink
+        to="/login"
+        className={({ isActive }) => (isActive ? "link active" : "link")}
+      >
+        Login
       </NavLink>
     </nav>
   );
 };
-
 export default Navbar;
